ae76589dce02db0bf2e35a7416cba3e33a6749ce
Lewis Sneddon <lewis@lewiscodes.io>
2021-07-17 14:35:28 +0100

    app css

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import { BrowserRouter, Route } from "react-router-dom";
 
 import './App.css';
 
-// import './bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 import NavBar from "./layout/header/navbar";
 import Certifications from "./components/certifications/Certifications";
