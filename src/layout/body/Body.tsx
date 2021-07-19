04ea570bb2a512b2513fe55c1b8ebb586b5d6da6
Lewis Sneddon <lewis@lewiscodes.io>
2021-07-19 10:11:39 +0100

    code splitting

--- src/layout/body/Body.tsx
+++ src/layout/body/Body.tsx
@@ -1,29 +1,44 @@
-import React, {} from 'react';
+import {lazy, Suspense} from 'react';
 import { BrowserRouter, Route, Switch } from "react-router-dom";
-import About from "../../components/about/About";
-import Projects from "../../components/projects/Projects";
-import Certifications from "../../components/certifications/Certifications";
-import Skills from "../../components/skills/Skills";
+import {Spinner} from "reactstrap";
+
+const About = lazy(() => import("../../components/about/About"));
+const Projects = lazy(() => import("../../components/projects/Projects"));
+const Certifications = lazy(() => import("../../components/certifications/Certifications"));
+const Skills = lazy(() => import("../../components/skills/Skills"));
 
 function Body() {
 	return (
 		<>
-			<BrowserRouter>
-				<Switch>
-					<Route path="/projects">
-						<Projects />
-					</Route>
-					<Route path="/certifications">
-						<Certifications />
-					</Route>
-					<Route path="/skills">
-						<Skills />
-					</Route>
-					<Route path="/">
-						<About />
-					</Route>
-				</Switch>
-			</BrowserRouter>
+			<Suspense fallback={
+				<div>
+					<Spinner type="grow" color="primary" />
+					<Spinner type="grow" color="secondary" />
+					<Spinner type="grow" color="success" />
+					<Spinner type="grow" color="danger" />
+					<Spinner type="grow" color="warning" />
+					<Spinner type="grow" color="info" />
+					<Spinner type="grow" color="light" />
+					<Spinner type="grow" color="dark" />
+				</div>
+			}>
+				<BrowserRouter>
+					<Switch>
+						<Route path="/projects">
+							<Projects />
+						</Route>
+						<Route path="/certifications">
+							<Certifications />
+						</Route>
+						<Route path="/skills">
+							<Skills />
+						</Route>
+						<Route path="/">
+							<About />
+						</Route>
+					</Switch>
+				</BrowserRouter>
+			</Suspense>
 		</>
 	);
 }
