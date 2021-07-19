2c62ba06d9de7a6d783d716e9147a72c708af62e
Lewis Sneddon <lewis@lewiscodes.io>
2021-07-19 08:31:28 +0100

    container to send mail

--- src/components/about/About.tsx
+++ src/components/about/About.tsx
@@ -1,15 +1,54 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Envelope} from "react-bootstrap-icons";
 import Icons from "../social/Icons";
 import List from "reactstrap/es/List";
+import ModalContainer from "../../modal/ModalContainer";
+import {Button, Modal, ModalBody} from "reactstrap";
 
 function About() {
+	const [state, setState] = useState({ showModal: false });
+
+	function toggleModal() {
+		console.log("toggle modal called");
+		setState({ showModal: !state.showModal });
+		console.log(state);
+	}
 	return (
 		<div className="m-5">
 			<h1>Lewis Sneddon</h1>
 			<div className="subheading mb-5">
 				Software Developer ·
-				<a href="mailto:lewis@lewiscodes.io"><Envelope /></a>
+				<Button
+					onClick={toggleModal}
+
+				><Envelope /></Button>
+				{
+					state.showModal ? (
+						<ModalContainer>
+							<div>
+								<div className="m-4">
+									Send an email with your default Mail app to lewis@lewiscodes.io ?
+								</div>
+								<div className="text-end">
+									<Button
+										className="m-2"
+										outline
+										size="sm"
+										color="warning"
+										onClick={toggleModal}
+									>Cancel</Button>
+									<Button
+										className="m-2"
+										outline
+										size="sm"
+										color="primary"
+										href="mailto:lewis@lewiscodes.io"
+									>Ok</Button>
+								</div>
+							</div>
+						</ModalContainer>
+					) : null
+				}
 			</div>
 			<Icons />
 			<List type="unstyled" className="m-5">
