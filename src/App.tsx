5ae76f195a3ab7c79e4e18566fee33ad8409c290
Lewis Sneddon <lewis@lewiscodes.io>
2021-08-16 13:23:21 +0100

    re-arranged projects

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,3 @@
-import React, {} from 'react';
-
 import './App.css';
 
 import 'bootstrap/dist/js/bootstrap.bundle.min';
--- src/components/projects/Projects.tsx
+++ src/components/projects/Projects.tsx
@@ -19,7 +19,7 @@ function Projects() {
 						className={classnames({ active: activeTab === '1' })}
 						onClick={() => { toggle('1'); }}
 					>
-						Web
+						Desktop
 					</NavLink>
 				</NavItem>
 				<NavItem>
@@ -27,21 +27,13 @@ function Projects() {
 						className={classnames({ active: activeTab === '2' })}
 						onClick={() => { toggle('2'); }}
 					>
-						Desktop
+						Web
 					</NavLink>
 				</NavItem>
 				<NavItem>
 					<NavLink
 						className={classnames({ active: activeTab === '3' })}
 						onClick={() => { toggle('3'); }}
-					>
-						Command Line
-					</NavLink>
-				</NavItem>
-				<NavItem>
-					<NavLink
-						className={classnames({ active: activeTab === '4' })}
-						onClick={() => { toggle('4'); }}
 					>
 						Mobile
 					</NavLink>
@@ -50,7 +42,7 @@ function Projects() {
 			<TabContent activeTab={activeTab}>
 				<TabPane tabId="1">
 					<div className="row align-content-center">
-						{webProjects.map((i) => {
+						{desktopProjects.map((i) => {
 							return (
 								<Project
 									title={i.title}
@@ -67,7 +59,7 @@ function Projects() {
 				</TabPane>
 				<TabPane tabId="2">
 					<div className="row">
-						{desktopProjects.map((i) => {
+						{webProjects.map((i) => {
 							return (
 								<Project
 									title={i.title}
@@ -83,23 +75,6 @@ function Projects() {
 					</div>
 				</TabPane>
 				<TabPane tabId="3">
-					<div className="row">
-						{terminalProjects.map((i) => {
-							return (
-								<Project
-									title={i.title}
-									description={i.description}
-									codeUrl={i.codeUrl}
-									launchUrl={i.launchUrl || ""}
-									techStack={i.techStack}
-									launchable={i.launchable}
-									key={i.title}
-								/>
-							);
-						})}
-					</div>
-				</TabPane>
-				<TabPane tabId="4">
 					<div className="row">
 						{mobileProjects.map((i) => {
 							return (
@@ -122,22 +97,22 @@ function Projects() {
 }
 
 const webProjects = [
-	{
-		title: "Code Bank",
-		description: "A Full Stack web app where users can share code snippets to common algorithm problems",
-		codeUrl: "https://github.com/codeslewis/code-bank",
-		launchable: false,
-		launchUrl: "",
-		techStack: ["java", "spring", "react", "postgresql"],
-	},
-	{
-		title: "Web Quiz App",
-		description: "Backed of a Spring Web App to create and solve quizzes.",
-		codeUrl: "https://github.com/codeslewis/CodeShare",
-		launchable: false,
-		launchUrl: "",
-		techStack: ["java", "spring"],
-	},
+	// {
+	// 	title: "Code Bank",
+	// 	description: "A Full Stack web app where users can share code snippets to common algorithm problems",
+	// 	codeUrl: "https://github.com/codeslewis/code-bank",
+	// 	launchable: false,
+	// 	launchUrl: "",
+	// 	techStack: ["java", "spring", "react", "postgresql"],
+	// },
+	// {
+	// 	title: "Web Quiz App",
+	// 	description: "Backed of a Spring Web App to create and solve quizzes.",
+	// 	codeUrl: "https://github.com/codeslewis/CodeShare",
+	// 	launchable: false,
+	// 	launchUrl: "",
+	// 	techStack: ["java", "spring"],
+	// },
 	{
 		title: "Bake Buddy",
 		description: "A conversion app for common baking ingredients.",
@@ -148,15 +123,36 @@ const webProjects = [
 	},
 	{
 		title: "Lewis Codes",
-		description: "This site. A serverless React SPA web app running on aws",
+		description: "This site. A React SPA",
 		codeUrl: "https://github.com/codeslewis/Portfolio_Site_React",
 		launchable: false,
 		launchUrl: "",
 		techStack: ["react", "ts", "node"],
+	}
+];
+
+const mobileProjects = [
+	{
+		title: "Bake Buddy (Android)",
+		description: `A baking ingredient conversion app. This Android app converts Imperial and US Legal baking ingredient measurements into the metric system.`,
+		codeUrl:
+			"https://github.com/codeslewis/Bake_Buddy_Android/tree/master/app/src",
+		launchable: false,
+		launchUrl: null,
+		techStack: ["android", "java"],
 	},
 ];
 
-const terminalProjects = [
+const desktopProjects = [
+	{
+		title: "Noughts and Crosses",
+		description: `Java Desktop Noughts and Crosses game using the swing library for GUI`,
+		codeUrl:
+			"https://github.com/codeslewis/DesktopNoughtsAndCrosses",
+		launchable: true,
+		launchUrl: "https://youtu.be/1PjB4aNRjBU",
+		techStack: ["java"],
+	},
 	{
 		title: "Minesweeper",
 		description: "Command Line Minesweeper game",
@@ -184,41 +180,24 @@ const terminalProjects = [
 		launchUrl: null,
 		techStack: ["java"],
 	},
+	// {
+	// 	title: "Noughts and Crosses",
+	// 	description: "An interactive game for one or two players",
+	// 	codeUrl:
+	// 		"https://github.com/codeslewis/Tic-Tac-Toe_with_AI/tree/master/Tic-Tac-Toe%20with%20AI/task/src/tictactoe",
+	// 	launchable: false,
+	// 	launchUrl: null,
+	// 	techStack: ["java"],
+	// },
 	{
-		title: "Noughts and Crosses",
-		description: "An interactive game for one or two players",
+		title: "Text Encryption",
+		description: "Encrypt and Decrypt text via files or command line using a shift or a unicode encryption algorithm",
 		codeUrl:
-			"https://github.com/codeslewis/Tic-Tac-Toe_with_AI/tree/master/Tic-Tac-Toe%20with%20AI/task/src/tictactoe",
+			"https://github.com/codeslewis/encryptor",
 		launchable: false,
 		launchUrl: null,
 		techStack: ["java"],
 	},
 ];
-
-const mobileProjects = [
-	{
-		title: "Bake Buddy (Android)",
-		description: `A baking ingredient conversion app. This Android app converts Imperial and US Legal baking ingredient measurements into the metric system.`,
-		codeUrl:
-			"https://github.com/codeslewis/Bake_Buddy_Android/tree/master/app/src",
-		launchable: false,
-		launchUrl: null,
-		techStack: ["android", "java"],
-	},
-];
-
-const desktopProjects = [
-	{
-		title: "Noughts and Crosses",
-		description: `Java Desktop Noughts and Crosses game using the swing library for GUI`,
-		codeUrl:
-			"https://github.com/codeslewis/DesktopNoughtsAndCrosses",
-		launchable: true,
-		launchUrl: "https://youtu.be/1PjB4aNRjBU",
-		techStack: ["java"],
-	},
-];
-
-
 
 export default Projects;
