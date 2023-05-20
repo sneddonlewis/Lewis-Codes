2c46985c05473c32f741028f50e382513922fabb
lewis sneddon <lewis@pop-os.localdomain>
2023-05-20 16:11:03 +0100

    Formatted string in dropdown

--- src/components/projects/SortSelector.tsx
+++ src/components/projects/SortSelector.tsx
@@ -10,13 +10,24 @@ export const SortSelector = (props: SortSelectorProps) => {
     const [dropdownOpen, setDropdownOpen] = useState(false);
 
     const toggle = () => setDropdownOpen((prevState) => !prevState);
+
+    const formatTechStr = (tech: string) => {
+        switch (tech) {
+            case "aws":
+                return "AWS";
+            case "ts":
+                return  "TypeScript";
+            default:
+                return `${tech.charAt(0).toUpperCase()}${tech.substring(1)}`
+        }
+    }
   
     return (
       <div className="">
         <Dropdown isOpen={dropdownOpen} toggle={toggle} direction={"down"}>
           <DropdownToggle caret>Sort by technology</DropdownToggle>
           <DropdownMenu>
-            {props.techs.map(item => <DropdownItem key={item} onClick={() => props.actionsOn(item)}>{item}</DropdownItem>)}
+            {props.techs.map(item => <DropdownItem key={item} onClick={() => props.actionsOn(item)}>{formatTechStr(item)}</DropdownItem>)}
           </DropdownMenu>
         </Dropdown>
       </div>
