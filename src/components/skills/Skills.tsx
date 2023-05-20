3a29e52f827df6258a99c70c763597208dbea57e
lewis sneddon <lewis@pop-os.localdomain>
2023-05-20 21:00:04 +0100

    Align headers on About page

--- src/components/skills/Skills.tsx
+++ src/components/skills/Skills.tsx
@@ -4,8 +4,8 @@ import { Container } from "reactstrap";
 function Skills() {
 	return (
 		<Container style={{ marginTop: "3rem" }}>
+			<h5>Some programming languages I know well</h5>
 			<ul>
-				<h5>Some programming languages I know well</h5>
 				<li className="list-inline-item">
 					{/* csharp */}
 					<img src={logos["csharp"]} alt="C# logo" />
@@ -24,8 +24,8 @@ function Skills() {
 					<img src={logos["typeScript"]} alt="TypeScript logo" />
 				</li>
 			</ul>
+			<h5>Frameworks I have experience using</h5>
 			<ul>
-				<h5>Frameworks I have experience using</h5>
 				<li className="list-inline-item">
 					<img src={logos["spring"]} alt="Spring logo" />
 				</li>
@@ -36,8 +36,8 @@ function Skills() {
 					<img src={logos["angular"]} alt="AngularJS logo" />
 				</li>
 			</ul>
+			<h5>Cloud providers whose services I use</h5>
 			<ul>
-				<h5>Cloud providers whose services I use</h5>
 				<li className="list-inline-item">
 					<img src={logos["azure"]} alt="Azure logo" />
 				</li>
@@ -45,8 +45,8 @@ function Skills() {
 					<img src={logos["aws"]} alt="aws logo" />
 				</li>
 			</ul>
-			 <h5>Verify my skills by reviewing my scores on <a  target="_blank" rel="noreferrer" href="https://app.pluralsight.com/profile/lewis-sneddon">Pluralsight</a></h5>
-			 <h5>See what I've been learning on <a target="_blank" rel="noreferrer" href="https://frontendmasters.com/u/lsneddon/">Frontend Masters</a></h5>
+			<h5>Verify my skills by reviewing my scores on <a  target="_blank" rel="noreferrer" href="https://app.pluralsight.com/profile/lewis-sneddon">Pluralsight</a></h5>
+			<h5>See what I've been learning on <a target="_blank" rel="noreferrer" href="https://frontendmasters.com/u/lsneddon/">Frontend Masters</a></h5>
 		</Container>
 	);
 }
