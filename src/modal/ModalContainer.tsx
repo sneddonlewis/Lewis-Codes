f9f48061febaa70b91e45c0c0f119875af0ce0a9
Lewis Sneddon <lewis@lewiscodes.io>
2024-05-24 20:49:52 +0100

    switched package manager npm -> yarn

--- src/modal/ModalContainer.tsx
+++ src/modal/ModalContainer.tsx
@@ -4,27 +4,27 @@ import {createPortal} from "react-dom";
 const modalRoot = document.getElementById('modal');
 
 function ModalContainer({ children }: { children: ReactNode}): ReactPortal {
-    const elRef: MutableRefObject<HTMLDivElement | any> = useRef(null);
-    if (!elRef.current) {
-        elRef.current = document.createElement('div');
+  const elRef: MutableRefObject<HTMLDivElement | any> = useRef(null);
+  if (!elRef.current) {
+    elRef.current = document.createElement('div');
+  }
+
+  useEffect(() => {
+    if (!modalRoot || !elRef.current) {
+      return;
     }
+    modalRoot.appendChild(elRef.current);
+    return () =>{
+      modalRoot.removeChild(elRef.current)
+    };
+  }, []);
 
-    useEffect(() => {
-        if (!modalRoot || !elRef.current) {
-            return;
-        }
-        modalRoot.appendChild(elRef.current);
-        return () =>{
-            modalRoot.removeChild(elRef.current)
-        };
-    }, []);
-
-    return createPortal(
-        <div>
-            {children}
-        </div>,
-        elRef.current
-    );
+  return createPortal(
+    <div>
+      {children}
+    </div>,
+    elRef.current
+  );
 }
 
 export default ModalContainer;
