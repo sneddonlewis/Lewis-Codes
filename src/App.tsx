547142e7b3d494a663fe1a91391031e488e277fb
Lewis Sneddon <lewis@lewiscodes.io>
2024-05-24 18:06:42 +0100

    removing nav bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,32 @@
 import './App.css';
 
 import 'bootstrap/dist/js/bootstrap.bundle.min';
-import NavBar from "./layout/header/navbar";
-import Body from "./layout/body/Body";
-import Footer from "./layout/footer/Footer";
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import About from './components/about/About';
+import Skills from './components/skills/Skills';
+import Certifications from './components/certifications/Certifications';
+import Projects from './components/projects/Projects';
 
 function App() {
   return (
-    <div className="App d-flex flex-column min-vh-100">
-        <NavBar />
-        <Body/>
-        <Footer />
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route path="/projects">
+          <Projects />
+        </Route>
+        <Route path="/certifications">
+          <Certifications />
+        </Route>
+        <Route path="/skills">
+          <Skills />
+        </Route>
+        <Route path="/">
+        <About />
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 }
+
 
 export default App;
