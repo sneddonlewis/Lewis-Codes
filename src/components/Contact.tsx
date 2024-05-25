6b59bdf3d142473bc8493eb7304ebc4560fa53ae
Lewis Sneddon <lewis@lewiscodes.io>
2024-05-25 17:21:33 +0100

    section size full viewport

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -3,7 +3,9 @@ export const Contact = () => {
   return (
     <>
       <h1>Contact Form</h1>
+      <div style={{ minHeight: '100vh' }}>
       <input type="text" />
+      </div>
     </>
   )
 }
--- src/components/Scrollspy.tsx
+++ src/components/Scrollspy.tsx
@@ -28,8 +28,9 @@ export const Scrollspy = () => {
     boxShadow: '0 2px 5px rgba(0,0,0,0.2)'
   }
 
-  const scrollSectionStyles = {
-    marginBottom: '6rem'
+  const scrollSectionContainer = {
+    minHeight: '100vh',
+    paddingTop: '6rem',
   }
 
   return (
@@ -44,6 +45,7 @@ export const Scrollspy = () => {
           style={navbarStyles}>
           <Navbar.Brand href="#">Lewis Sneddon</Navbar.Brand>
             <Nav className="ms-auto">
+              <Nav.Link href="#skills">Skills</Nav.Link>
               <Nav.Link href="#projects">Projects</Nav.Link>
               <Nav.Link href="#contact">Contact</Nav.Link>
             </Nav>
@@ -57,19 +59,22 @@ export const Scrollspy = () => {
           className="p-3 rounded-2"
           tabIndex={0}>
 
-          <div style={scrollSectionStyles}></div>
-          <About  />
+          <div style={scrollSectionContainer}>
+            <About  />
+          </div>
 
-          <div style={scrollSectionStyles}></div>
-          <Skills />
-          <Certifications />
+          <div id="skills" style={scrollSectionContainer}>
+            <Skills />
+            <Certifications />
+          </div>
 
+          <div id="projects" style={scrollSectionContainer}>
+            <ProjectList />
+          </div>
 
-          <div id="projects" style={scrollSectionStyles}></div>
-          <ProjectList />
-
-          <div id="contact" style={scrollSectionStyles}></div>
-          <Contact />
+          <div id="contact" style={scrollSectionContainer}>
+            <Contact />
+          </div>
 
         </div> 
       </Container>
