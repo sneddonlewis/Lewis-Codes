d66a191d16b0802387516c80a2ba67f5194a440f
Lewis Sneddon <lewis@lewiscodes.io>
2024-05-25 14:13:16 +0100

    scroll spy

--- src/components/Scrollspy.tsx
+++ src/components/Scrollspy.tsx
@@ -1,37 +1,38 @@
+import About from "./About"
+import Certifications from "./Certifications"
+import Projects from "./Projects"
+import Skills from "./Skills"
 
 export const Scrollspy = () => {
   return (
     <>
-      <nav id="navbar-example2" className={`navbar bg-body-tertiary px-3 mb-3`}>
-        <a className="navbar-brand" href="#">Navbar</a>
-        <ul className="nav nav-pills">
+      <nav id="sn-navbar" className={`navbar fixed-top px-3 mb-3`}>
+        <a className="navbar-brand" href="#">Lewis Sneddon</a>
+        <ul className={`nav nav-pills`}>
           <li className="nav-item">
-            <a className="nav-link" href="#scrollspyHeading1">First</a>
+            <a className="nav-link" href="#scrollspyHeading1">Skills</a>
           </li>
           <li className="nav-item">
-            <a className="nav-link" href="#scrollspyHeading2">Second</a>
-          </li>
-          <li className={`nav-item dropdown`}>
-            <a className={`nav-link dropdown-toggle`} data-bs-toggle="dropdown" href="#" role="button" aria-expanded="false">Dropdown</a>
-            <ul className="dropdown-menu">
-              <li><a className="dropdown-item" href="#scrollspyHeading3">Third</a></li>
-              <li><a className="dropdown-item" href="#scrollspyHeading4">Fourth</a></li>
-              <li><a className="dropdown-item" href="#scrollspyHeading5">Fifth</a></li>
-            </ul>
+            <a className="nav-link" href="#scrollspyHeading2">Projects</a>
           </li>
         </ul>
       </nav>
-      <div data-bs-spy="scroll" data-bs-target="#navbar-example2" data-bs-root-margin="0px 0px -40%" data-bs-smooth-scroll="true" class="scrollspy-example bg-body-tertiary p-3 rounded-2" tabindex="0">
-        <h4 id="scrollspyHeading1">First heading</h4>
-        <p>...</p>
-        <h4 id="scrollspyHeading2">Second heading</h4>
-        <p>...</p>
-        <h4 id="scrollspyHeading3">Third heading</h4>
-        <p>...</p>
-        <h4 id="scrollspyHeading4">Fourth heading</h4>
-        <p>...</p>
-        <h4 id="scrollspyHeading5">Fifth heading</h4>
-        <p>...</p>
+      <div
+        data-bs-spy="scroll" 
+          data-bs-target='#sn-navbar'
+        data-bs-root-margin="0px 0px -40%"
+        data-bs-smooth-scroll="true" 
+        className={`p-3 rounded-2`}
+        tabindex='0'>
+        <h4>About</h4>
+        <About  />
+
+        <h4 id="scrollspyHeading1">Skills</h4>
+        <Skills />
+        <Certifications />
+
+        <h4 id="scrollspyHeading3">Projects</h4>
+        <Projects />
       </div> 
     </>
   )
