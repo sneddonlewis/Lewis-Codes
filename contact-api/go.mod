178d4f1b8fe44724f197846a4a9c85697d868ce3
Lewis Sneddon <lewis@lewiscodes.io>
2024-05-26 14:33:25 +0100

    health check endpoint

--- contact-api/go.mod
+++ contact-api/go.mod
@@ -1,3 +1,5 @@
 module contact.sneddsy.com
 
 go 1.21.3
+
+require github.com/aws/aws-lambda-go v1.47.0 // indirect
--- contact-api/main.go
+++ contact-api/main.go
@@ -1,5 +1,30 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"net/http"
 
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+)
+
+func main() {
+	lambda.Start(func(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+		switch request.Path {
+		case "/health":
+			return health(request)
+		default:
+			return events.APIGatewayProxyResponse{
+				StatusCode: http.StatusMethodNotAllowed,
+			}, nil
+		}
+	})
+
+}
+
+func health(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	return events.APIGatewayProxyResponse{
+		Body:       fmt.Sprint("Success"),
+		StatusCode: http.StatusOK,
+	}, nil
 }
