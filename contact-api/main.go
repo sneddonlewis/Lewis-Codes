c7ae0567378bf731db4560e558ad6a58e5f300ed
Lewis Sneddon <lewis@lewiscodes.io>
2024-05-26 15:12:04 +0100

    message endpoint just returns request

--- contact-api/main.go
+++ contact-api/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 
@@ -13,6 +14,8 @@ func main() {
 		switch request.Path {
 		case "/health":
 			return health(request)
+		case "/message":
+			return message(request)
 		default:
 			return events.APIGatewayProxyResponse{
 				StatusCode: http.StatusMethodNotAllowed,
@@ -24,7 +27,53 @@ func main() {
 
 func health(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
 	return events.APIGatewayProxyResponse{
-		Body:       fmt.Sprint("Success"),
+		Body:       "success",
 		StatusCode: http.StatusOK,
 	}, nil
+}
+
+func message(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	var messageRequest Message
+
+	err := json.Unmarshal([]byte(request.Body), &messageRequest)
+	if err != nil {
+		return events.APIGatewayProxyResponse{
+			Body:       "invalid request",
+			StatusCode: http.StatusBadRequest,
+		}, err
+	}
+
+	// validation
+	if messageRequest.Email == "" || messageRequest.Message == "" {
+		return events.APIGatewayProxyResponse{
+			Body:       "require email and message",
+			StatusCode: http.StatusUnprocessableEntity,
+		}, fmt.Errorf("request incorrectly formed")
+	}
+
+	response := struct {
+		MessageStatus string  `json:"message_status"`
+		MessageBody   Message `json:"message_body"`
+	}{
+		MessageStatus: "server is still being developed so this message is returned unprocessed",
+		MessageBody:   messageRequest,
+	}
+
+	jsonResponse, err := json.Marshal(response)
+	if err != nil {
+		return events.APIGatewayProxyResponse{
+			Body:       "server failure",
+			StatusCode: http.StatusInternalServerError,
+		}, fmt.Errorf("server failure")
+	}
+
+	return events.APIGatewayProxyResponse{
+		Body:       string(jsonResponse),
+		StatusCode: http.StatusOK,
+	}, nil
+}
+
+type Message struct {
+	Email   string `json:"email"`
+	Message string `json:"message"`
 }
