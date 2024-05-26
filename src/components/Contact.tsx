50a423fadbc54b3163c5f1537faa1985367b63dd
Lewis Sneddon <lewis@lewiscodes.io>
2024-05-26 09:10:04 +0100

    fixed form validation

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,65 +1,51 @@
 import React, { FormEvent, useState } from "react"
 import { Button, Form } from "react-bootstrap"
 
-type FormErrors = {
-  email?: string,
-  msg?: string,
-}
-
 export const Contact: React.FC = () => {
   const [msg, setMsg] = useState('')
   const [email, setEmail] = useState('')
-  const [errors, setErrors] = useState<FormErrors>({ email: undefined, msg: undefined })
+
+  const [emailError, setEmailError] = useState<string | undefined>(undefined)
+  const [msgError, setMsgError] = useState<string | undefined>(undefined)
 
   const msgId = 'contactFormMessageId'
   const emailId = 'contactFormEmailId'
   const basicEmailRegex = new RegExp(/\S+@\S+\.\S+/)
 
-  const validate = (emailAddr: string, message: string) => {
-    let isValid = true
-    setErrors(prevErrors => {
-      const errors = prevErrors
-      if (!emailAddr) {
-        errors.email = 'I need your email address'
-        isValid = false
-      } else if (!basicEmailRegex.test(email)) {
-        errors.email = "That's not a valid email address";
-        isValid = false
-      } else {
-        errors.email = undefined
-      }
-      if (!message) {
-        errors.msg = 'Oops, looks like you forgot to add your message'
-        isValid = false
-      } else {
-        errors.msg = undefined
-      }
-      return errors
-    })
-    return isValid
-  }
-
-
   const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
     if (e.target.id === emailId) {
       setEmail(e.target.value)
-      setErrors(prevErrors => ({ ...prevErrors, email: undefined }));
     }
     if (e.target.id === msgId) {
       setMsg(e.target.value)
-      setErrors(prevErrors => ({ ...prevErrors, msg: undefined }));
     }
   }
 
   const handleSubmit = (e: FormEvent) => {
-    e.preventDefault();
-    setTimeout(() => {
-      const isValid = validate(email, msg)
-      if (!isValid) {
-        return
-      }
-      console.log(`Time for a POST request with Email: ${email} & Message: ${msg}`)
-    }, 0)
+    e.preventDefault()
+    let invalid = false
+
+    if (!email) {
+      setEmailError('I need your email address')
+      invalid = true
+    } else if (!basicEmailRegex.test(email)) {
+      setEmailError("That's not a valid email address")
+      invalid = true
+    } else {
+      setEmailError(undefined)
+    }
+    if (!msg) {
+      setMsgError('Oops, looks like you forgot to add your message')
+      invalid = true
+    } else {
+      setMsgError(undefined)
+    }
+    if (invalid) {
+      console.log('invalid')
+      return
+    }
+
+    console.log(`Time for a POST request with Email: ${email} & Message: ${msg}`)
   }
 
 
@@ -75,7 +61,7 @@ export const Contact: React.FC = () => {
             value={email}
             onChange={handleChange}
           />
-          {errors.email && <small className="text-danger">{errors.email}</small>}
+          {emailError && <small className="text-danger">{emailError}</small>}
         </Form.Group>
 
         <Form.Group className="mb-3" controlId={msgId}>
@@ -86,7 +72,7 @@ export const Contact: React.FC = () => {
             value={msg}
             onChange={handleChange}
           />
-          {errors.msg && <small className="text-danger">{errors.msg}</small>}
+          {msgError && <small className="text-danger">{msgError}</small>}
         </Form.Group>
 
         <Button
