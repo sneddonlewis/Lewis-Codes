070ffd0725500f310e66e16456a940ea81562f48
Lewis Sneddon <lewis@sneddsy.com>
2024-06-02 10:12:40 +0100

    corrected typo in about section

--- ui/src/components/About.tsx
+++ ui/src/components/About.tsx
@@ -8,7 +8,7 @@ export const About = () => {
 				<h2>Lewis Sneddon</h2>
         <Icons />
 				<p className="mt-3">Welcome to my site, I'm Lewis and I'm a full stack developer.</p>
-        <p>I love building software, jump straigt to <a href="#projects">Projects</a> to see some of the things I've been working on.</p>
+        <p>I love building software, jump straight to <a href="#projects">Projects</a> to see some of the things I've been working on.</p>
         <Certifications />
 			</div>
 		</>
